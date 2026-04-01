13485edb6d3ab1548a2b2b354c74f246038da14e
Mohamed Ben Slama <MedBenSlama@users.noreply.github.com>
2026-04-01 17:06:25 +0100

    adding files v3

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,3 +4,7 @@ export const axiosInstance = axios.create({
   baseURL: "https://tunibackend.onrender.com/api", // always backend URL
   withCredentials: true, // only needed if using cookies
 });
+
+axiosInstance.get("/user/profile")
+  .then(res => console.log(res.data))
+  .catch(err => console.error(err));
